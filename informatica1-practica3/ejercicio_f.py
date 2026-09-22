29f208426e5557761e044c6254662fa039e059e2
Jaime Montoya <innovasistemas@gmail.com>
2026-09-21 21:17:11 -0500

    Informática I. Práctica 3. Ejercicios f)

---
+++ informatica1-practica3/ejercicio_f.py
@@ -0,0 +1,23 @@
+# ----------------------------------------
+# Programa: Ejercicio f) Triángulo rectángulo
+# ----------------------------------------
+
+# ----------------------------------------
+# Definición de variables
+# ----------------------------------------
+# h: Altura del triángulo rectángulo
+
+h = int(input("Altura del triángulo rectángulo: "))
+if h > 0:
+    print("\033[33m", end = "")
+    for i in range(1, h + 1, 1):
+        print(" " * (h - i), end="")
+        print("*" * i)
+    print("\033[0m", end = "")
+else:
+    print("\n")
+    print("\033[31m")
+    print("-" * 38)
+    print("¡Error! La altua debe ser mayor a cero")
+    print("-" * 38)
+    print("\033[0m")
